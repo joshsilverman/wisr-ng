b5a1cfbc040bf5d633de333050ee89fa0b88adca
Josh Silverman <joshs.silverman@gmail.com>
2014-08-02 11:28:27 -0400

    take in real data

--- app/scripts/resources/publications.js
+++ app/scripts/resources/publications.js
@@ -5,6 +5,6 @@
 angular.module('wisrNgApp').factory('Publications', ['$resource',
   function($resource) {
     // return $resource('articles/:articleId', {
-    return $resource('https://www.wisr.com/biology.json', {}, {});
+    return $resource('http://data.localhost:8080/biology.json', {}, {});
   }
 ]);
