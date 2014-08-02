5ba8fb61b0395042c2af2e623099ffb0976e09a6
Josh Silverman <joshs.silverman@gmail.com>
2014-08-02 11:41:32 -0400

    urls working with multiple subjects

--- app/scripts/controllers/feed.js
+++ app/scripts/controllers/feed.js
@@ -8,10 +8,10 @@
  * Controller of the wisrNgApp
  */
 angular.module('wisrNgApp')
-  .controller('FeedCtrl', function ($scope, Publications, Paths) {
+  .controller('FeedCtrl', function ($scope, $routeParams, Publications, Paths) {
     $scope.assetBasePath = Paths.assets;
 
-    Publications.query({}, function(data) {
+    Publications.query({subjectURL: $routeParams['subjectURL']}, function(data) {
         $scope.publications = data;
     });
   });
--- app/scripts/resources/publications.js
+++ app/scripts/resources/publications.js
@@ -4,7 +4,6 @@
 //Articles service used for articles REST endpoint
 angular.module('wisrNgApp').factory('Publications', ['$resource',
   function($resource) {
-    // return $resource('articles/:articleId', {
-    return $resource('http://data.localhost:8080/biology.json', {}, {});
+    return $resource('http://data.localhost:8080/:subjectURL.json', {}, {});
   }
 ]);
