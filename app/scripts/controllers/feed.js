197f7d4f60a6e51ddfcadb92c2e71e2684a95d90
Josh Silverman <joshs.silverman@gmail.com>
2014-08-09 07:06:35 -0400

    refactor broadcast call

--- app/scripts/controllers/feed.js
+++ app/scripts/controllers/feed.js
@@ -9,13 +9,15 @@
  */
 angular.module('wisrNgApp')
   .controller('FeedCtrl', function ($scope, $routeParams, $timeout, PublicationsRsrc, Paths, CurrentUser, CorrectQuestionIdsRsrc) {
-    var currentUser, correctQIds, offset, 
-      loadingPublications, loadingCorrectAIds;
+    var currentUser, offset, loadingPublications;
 
     var init = function() {
       $scope.assetBasePath = Paths.assets;
       $scope.publications = [];
       offset = 0;
+
+      $scope.$watch('correctQIds', broadcastCorrectAnswersLoaded);
+      $scope.$watch('publications', broadcastCorrectAnswersLoaded);
 
       CurrentUser(function(_currentUser) {
         currentUser = _currentUser;
@@ -29,26 +31,23 @@ angular.module('wisrNgApp')
       PublicationsRsrc.query({subjectURL: $routeParams.subjectURL, offset: offset}, 
         function(data) {
           $scope.publications = $scope.publications.concat(data);
-          $timeout(loadCorrectAnswers);
           loadingPublications = false;
       });
     };
 
     var loadCorrectAnswers = function() {
-      var callback = function() {
-        $scope.$broadcast('FeedCtrl:correctQIds:loaded', correctQIds);
-      }
-
-      if (!correctQIds && !loadingCorrectAIds) {
-        loadingCorrectAIds = true;
-        CorrectQuestionIdsRsrc.query({currentUserId: currentUser.id}, 
-          function(_correctQIds) {
-            correctQIds = _correctQIds;
-            callback();
-            loadingCorrectAIds = false;
-        });
-      } else {callback();}
+      CorrectQuestionIdsRsrc.query({currentUserId: currentUser.id}, 
+        function(correctQIds) {
+          $scope.correctQIds = correctQIds;
+      });
     };
+
+    var broadcastCorrectAnswersLoaded = function() {
+      if (!$scope.correctQIds) return;
+      $timeout(function() {
+        $scope.$broadcast('FeedCtrl:correctQIds:loaded', $scope.correctQIds);
+      });
+    }
 
     $scope.loadMore = function() {
       if (loadingPublications) {return;}
