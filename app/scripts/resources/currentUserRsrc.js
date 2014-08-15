c932f9a9c1d542d5cdcd4068876d1e1ac7c42a13
Josh Silverman <joshs.silverman@gmail.com>
2014-08-15 09:08:04 -0400

    bad comment cleanup

--- app/scripts/resources/currentUserRsrc.js
+++ app/scripts/resources/currentUserRsrc.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// console.log('publications');
-//Articles service used for articles REST endpoint
 angular.module('wisrNgApp').factory('CurrentUserRsrc', function($resource, Paths) {
     return $resource(Paths.apiURL + '/users/me.json', {}, {});
   }
