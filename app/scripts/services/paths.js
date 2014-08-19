6aa3138189a81c0da20b23d72b48f94648a5b6fb
Josh Silverman <joshs.silverman@gmail.com>
2014-08-19 19:26:29 -0400

    modify wisr production base path

--- app/scripts/services/paths.js
+++ app/scripts/services/paths.js
@@ -14,14 +14,14 @@ angular.module('wisrNgApp')
         baseURL: 'http://dev.localhost'
       },
       production: {
-        baseURL: 'http://wisr.com'
+        baseURL: 'https://www.wisr.com'
       }
     };
 
     return { 
       'assets': 'https://wisr-herokuapp-com.global.ssl.fastly.net/assets',
       'imageBaseURL': envDependentPaths[ENV.name].baseURL + '/assets',
-      'authBaseURL': 'https:/www.wisr.com',
+      'authBaseURL': 'https://www.wisr.com',
       'apiURL': envDependentPaths[ENV.name].baseURL,
       'legacyURL': envDependentPaths[ENV.name].baseURL };
   });
