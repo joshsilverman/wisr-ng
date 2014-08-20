a9ca95efedbfb3ed4b897097a03c6b10b8464b2c
Josh Silverman <joshs.silverman@gmail.com>
2014-08-19 22:21:22 -0400

    another attempt at forcing ssl

--- web.js
+++ web.js
@@ -5,5 +5,15 @@ var app = express();
 
 app.use(logfmt.requestLogger());
  
+function requireHTTPS(req, res, next) {
+    if (!req.secure) {
+        //FYI this should work for local development as well
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+app.use(requireHTTPS);
+ 
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
