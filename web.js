79ba79879844baa0a3ba9b03b08fd8e1ae234c08
Josh Silverman <joshs.silverman@gmail.com>
2014-08-19 22:15:28 -0400

    Revert "attempt force ssl package"
    This reverts commit f63b8b547d354d27957572592be1d7aacf5cd202.

--- web.js
+++ web.js
@@ -1,13 +1,9 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var logfmt = require("logfmt");
-var forceSSL = require('express-force-ssl');
-
 var app = express();
 
 app.use(logfmt.requestLogger());
  
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.use(forceSSL);
-
 app.listen(process.env.PORT || 5000);
