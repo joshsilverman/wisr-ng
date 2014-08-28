a3e93a59ba4ca76dbdf6821d09517fdb270b80d2
Josh Silverman <joshs.silverman@gmail.com>
2014-08-28 09:17:23 -0400

    answered counts update after answering a questions

--- app/scripts/controllers/lessons.js
+++ app/scripts/controllers/lessons.js
@@ -8,17 +8,22 @@
  * Controller of the wisrNgApp
  */
 angular.module('wisrNgApp')
-  .controller('LessonsCtrl', function ($scope, LessonsRsrc, LessonAnswerCountsRsrc) {
+  .controller('LessonsCtrl', function ($scope, $rootScope, LessonsRsrc, LessonAnswerCountsRsrc) {
     var init = function() {
       LessonsRsrc.get({asker_id: 13588}, function(data) {
         $scope.lessons = data.topics;
         $scope.subject_url = data.meta.subject_url;
       });
 
+      updateAnsweredCounts();
+      $rootScope.$on('AnswerCtrl:correct', updateAnsweredCounts);
+    };
+
+    var updateAnsweredCounts = function() {
       LessonAnswerCountsRsrc.get({}, function(data) {
         $scope.lessonCounts = data;
       });
-    };
+    }
 
     init();
   });
