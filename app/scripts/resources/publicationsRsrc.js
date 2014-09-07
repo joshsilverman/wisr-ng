6168eb50adec83d4ada457eb5333778306d5eb67
Josh Silverman <joshs.silverman@gmail.com>
2014-09-07 15:13:45 -0400

    whitespace

--- app/scripts/resources/publicationsRsrc.js
+++ app/scripts/resources/publicationsRsrc.js
@@ -2,6 +2,6 @@
 
 //Publications service used for publications REST endpoint
 angular.module('wisrNgApp').factory('PublicationsRsrc', function($resource, Paths) {
-    return $resource(Paths.apiURL + '/:subjectURL.json?offset=:offset', 
-      {subjectURL: 'index'}, {});
-  });
+  return $resource(Paths.apiURL + '/:subjectURL.json?offset=:offset',
+    {subjectURL: 'index'}, {});
+});
