d6a161621b4d3d6a51b109f078aee43111338d92
Josh Silverman <joshs.silverman@gmail.com>
2014-09-07 14:56:54 -0400

    another rev on grunt file asset inclusion

--- Gruntfile.js
+++ Gruntfile.js
@@ -203,7 +203,7 @@ module.exports = function (grunt) {
       },
       sass: {
         src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-        ignorePath: /(\.\.){1,2}bower_components\//
+        ignorePath: /(\.\.\/){1,2}bower_components\//
       }
     },
 
