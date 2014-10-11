175ff85986369418fe010d6e73bdaa057821c96d
Josh Silverman <joshs.silverman@gmail.com>
2014-10-11 13:20:50 -0400

    translate answered counts to scope

--- app/scripts/controllers/publication_meta_info.js
+++ app/scripts/controllers/publication_meta_info.js
@@ -10,8 +10,40 @@
 angular.module('wisrNgApp')
   .controller('PublicationMetaInfoCtrl', function ($scope) {
     function init() {
+      setAuthorInfo();
+      setCounts();
+    }
+
+    function setAuthorInfo() { 
+      if (!$scope.publication) return;
+      if (!$scope.publication._question) return;
+
       $scope.author = $scope.publication._question.author_twi_screen_name
       $scope.created_at = $scope.publication._question.created_at
+    }
+
+    function setCounts() {
+      var correctCount = 0,
+        incorrectCount = 0;
+
+      $scope.answered_count = 0;
+      $scope.percent_correct = 0;
+
+      if (!$scope.publication) return;
+      if (!$scope.publication._answer_counts) return;
+
+      if ($scope.publication._answer_counts.correct) {
+        correctCount = parseInt($scope.publication._answer_counts.correct);
+        $scope.answered_count += correctCount;
+      }
+
+      if ($scope.publication._answer_counts.incorrect) {
+        incorrectCount = parseInt($scope.publication._answer_counts.incorrect);
+        $scope.answered_count += incorrectCount;
+      }
+
+      if ((incorrectCount + correctCount) > 0)
+        $scope.percent_correct = correctCount / (incorrectCount + correctCount) * 100;
     }
 
     init();
--- test/spec/controllers/publication_meta_info.js
+++ test/spec/controllers/publication_meta_info.js
@@ -1,20 +1,103 @@
 'use strict';
 
 describe('Controller: PublicationMetaInfoCtrl', function () {
-
-  // load the controller's module
   beforeEach(module('wisrNgApp'));
 
   var PublicationMetaInfoCtrl,
-    scope;
+    scope, __controller__, __rootScope__;
 
   beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    PublicationMetaInfoCtrl = $controller('PublicationMetaInfoCtrl', {
-      $scope: scope
-    });
+    __controller__ = $controller
+    __rootScope__ = $rootScope
   }));
 
-  // it('should attach a list of awesomeThings to the scope', function () {
-  // });
+  it('should attach authored info directly to scope', function () {
+    scope = __rootScope__.$new();
+    scope.publication = {
+      _question: {
+        author_twi_screen_name: 'abe',
+        created_at: 'today'
+      }
+    }
+
+    PublicationMetaInfoCtrl = __controller__('PublicationMetaInfoCtrl', {
+      $scope: scope
+    });
+
+    expect(scope.author).toEqual('abe');
+    expect(scope.created_at).toEqual('today');
+  });
+
+  it('wont error if author unavailable', function () {
+    scope = __rootScope__.$new();
+    PublicationMetaInfoCtrl = __controller__('PublicationMetaInfoCtrl', {
+      $scope: scope
+    });
+
+    expect(scope.author).toEqual(undefined);
+  });
+
+  it('should attach counts', function () {
+    scope = __rootScope__.$new();
+    scope.publication = {
+      _answer_counts: {
+        correct: '25',
+        incorrect: '75'
+      }
+    }
+
+    PublicationMetaInfoCtrl = __controller__('PublicationMetaInfoCtrl', {
+      $scope: scope
+    });
+
+    expect(scope.answered_count).toEqual(100);
+  });
+
+  it('should attach aggregate counts when one is 0', function () {
+    scope = __rootScope__.$new();
+    scope.publication = {
+      _answer_counts: {
+        correct: '25',
+        incorrect: '0'
+      }
+    }
+
+    PublicationMetaInfoCtrl = __controller__('PublicationMetaInfoCtrl', {
+      $scope: scope
+    });
+
+    expect(scope.answered_count).toEqual(25);
+  });
+
+  it('should attach percent correct', function () {
+    scope = __rootScope__.$new();
+    scope.publication = {
+      _answer_counts: {
+        correct: '25',
+        incorrect: '75'
+      }
+    }
+
+    PublicationMetaInfoCtrl = __controller__('PublicationMetaInfoCtrl', {
+      $scope: scope
+    });
+
+    expect(scope.percent_correct).toEqual(25);
+  });
+
+  it('should attach percent correct as 0 when nothing answered', function () {
+    scope = __rootScope__.$new();
+    scope.publication = {
+      _answer_counts: {
+        correct: '0',
+        incorrect: '0'
+      }
+    }
+
+    PublicationMetaInfoCtrl = __controller__('PublicationMetaInfoCtrl', {
+      $scope: scope
+    });
+
+    expect(scope.percent_correct).toEqual(0);
+  });
 });
