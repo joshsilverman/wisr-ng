f16890e2660bd086de123d7ca966f4208727fce3
Josh Silverman <joshs.silverman@gmail.com>
2014-11-23 18:22:12 -0500

    auto run tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -418,7 +418,7 @@ module.exports = function (grunt) {
     karma: {
       unit: {
         configFile: 'test/karma.conf.js',
-        singleRun: true
+        autoWatch: true
       }
     }
   });
