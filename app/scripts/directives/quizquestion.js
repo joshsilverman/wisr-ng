7a208038f77d2617a1c53bbf75ef0317674b2c4e
Josh Silverman <joshs.silverman@gmail.com>
2014-11-24 19:48:37 -0500

    all tests passing

--- app/scripts/directives/quizquestion.js
+++ app/scripts/directives/quizquestion.js
@@ -8,27 +8,50 @@
  */
 angular.module('wisrNgApp')
   .directive('quizquestion', function () {
-    function link(scope, element, attrs) {
-      var correctAnswerId = scope.question._question.correct_answer_id;
+    var scope;
+
+    function link(_scope, element, attrs) {
+      scope = _scope;
+      // console.log(scope.question);
+      // console.log(scope.question['_answers']);
+      assignAnswers();
+    }
+
+    function assignAnswers() {
+      if (!scope.question || !scope.question['_answers']) return;
+
+      var correctAnswerId = scope.question['_question'].correct_answer_id;
       if (correctAnswerId) {
         scope.correctAnswer = {
-          text: scope.question._answers[correctAnswerId]
+          text: scope.question['_answers'][correctAnswerId]
         };
       }
+
+      scope.incorrectAnswers = [];
+      _.each(scope.question['_answers'], function(questionText, id) {
+        if (id == correctAnswerId) return;
+
+        scope.incorrectAnswers.push({
+          id: id,
+          text: questionText
+        });
+      });
     }
 
     return {
       templateUrl: '/views/quizmaker/_new_question.html',
       restrict: 'E',
       scope: {
-        question: '='
+        question: '=',
+        correctAnswer: '=',
+        incorrectAnswers: '='
       },
-      link: function(scope, element, attrs) {
-        scope.$watch('question', function() {
-          link(scope, element, attrs);          
+      link: function(_scope, element, attrs) {
+        _scope.$watch('question', function() {
+          link(_scope, element, attrs);          
         });
 
-        link(scope, element, attrs);
+        link(_scope, element, attrs);
       }
     };
   });
